8cc7a1da9947f8333efabcbd84e9aa6de3862272
wtanjo <mail@wtanjo.cc>
2026-09-10 11:49:12 +0800

    disable truncate lines in dired-mode

--- lisp/facade.el
+++ lisp/facade.el
@@ -10,6 +10,8 @@
 (setq display-line-numbers-type 'relative)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-hook 'prog-mode-hook (lambda () (setq truncate-lines t)))
+(add-hook 'dired-mode-hook (lambda () (setq truncate-lines nil)))
+(add-hook 'text-mode-hook #'visual-line-mode)
 (add-hook 'org-mode-hook #'visual-line-mode)
 (add-hook 'markdown-mode-hook #'visual-line-mode)
 (global-goto-address-mode 1)
